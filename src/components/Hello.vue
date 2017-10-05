e6a753a37c41bb6b74e848daf6a8b2a4025f8305
Wesley <wesley100002001@gmail.com>
2017-10-05 16:02:09 +0800

    Import bootstrap for styling

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-    <input type="text" placeholder="Enter Sheet ID" @v-model="sheetID">
-    <button id="authorize-button" v-if="showAuth" @click="handleAuthClick()">Authorize</button>
-    <button id="signout-button" v-if="showSignout" @click="handleSignoutClick()">Sign Out</button>
-    <button @click="appStart()">appStart</button>
+    <h1>Core Batch Import</h1>
+    <div class="row">
+      <div class="col-md-4">
+      </div>
+      <div class="col-md-4">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Enter Sheet ID" @v-model="sheetID">
+        </div>
+        <button class="btn btn-default" v-if="showAuth" @click="handleAuthClick()">Authorize</button>
+        <button class="btn btn-default" v-if="showSignout" @click="handleSignoutClick()">Sign Out</button>
+        <button class="btn btn-default" @click="appStart()">appStart</button>
+      </div>
+    </div>
   </div>
 </template>
 
